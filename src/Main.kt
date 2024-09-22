b38704d711d51499e26b8630cd4ec6368d084b95
Admin <reteamboss@gmail.com>
2024-09-22 22:00:43 +0300

    1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,7 @@
 fun main() {
-    println("Hello World!")
+    val colorsEng: List<String> = arrayListOf("red","green","balck","white")
+    val colorsRus: List<String> = arrayListOf("красный","зеленый","черный","белый")
+    println(colorsEng.zip(colorsRus){colorEng,colorRus ->
+        "Значение: $colorEng, Перевод:$colorRus"
+    })
 }
